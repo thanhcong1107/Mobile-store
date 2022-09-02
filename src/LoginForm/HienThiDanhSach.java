f271d71c317d37ebe04e323c2eee2b6b57334b10
thanhcong1107 <92775736+thanhcong1107@users.noreply.github.com>
2022-09-02 22:06:06 +0700

    Update HienThiDanhSach.java

--- src/LoginForm/HienThiDanhSach.java
+++ src/LoginForm/HienThiDanhSach.java
@@ -27,63 +27,63 @@ public class HienThiDanhSach extends JFrame
 
   public HienThiDanhSach()
   {
-    jf.setLayout(null);
+    	jf.setLayout(null);
   	ln = new JLabel("Danh sách nhân viên");
-    ln.setFont(new Font("Times New Roman",Font.BOLD,25));
-    ln.setForeground(Color.blue);
-    ln.setBounds(300,30,300,25);
-    jf.add(ln);
+    	ln.setFont(new Font("Times New Roman",Font.BOLD,25));
+    	ln.setForeground(Color.blue);
+    	ln.setBounds(300,30,300,25);
+    	jf.add(ln);
 
-    scrlPane.setBounds(0,80,900,600);
-    jf.add(scrlPane);
-    tabGrid.setFont(new Font ("Times New Roman",0,14));
+    	scrlPane.setBounds(0,80,900,600);
+    	jf.add(scrlPane);
+    	tabGrid.setFont(new Font ("Times New Roman",0,14));
 
-    model.addColumn("Tên nhân viên");
-    model.addColumn("Ngày sinh");
-    model.addColumn("Số điện thoại");
-    model.addColumn("Địa chỉ");
-    model.addColumn("Mã nhân viên");
-    model.addColumn("Chức vụ");
-    model.addColumn("Mã chức vụ");
-    model.addColumn("Tài khoản");
-    model.addColumn("Mật khẩu");
-    model.addColumn("Giới tính");
+    	model.addColumn("Tên nhân viên");
+    	model.addColumn("Ngày sinh");
+    	model.addColumn("Số điện thoại");
+    	model.addColumn("Địa chỉ");
+    	model.addColumn("Mã nhân viên");
+    	model.addColumn("Chức vụ");
+    	model.addColumn("Mã chức vụ");
+    	model.addColumn("Tài khoản");
+    	model.addColumn("Mật khẩu");
+    	model.addColumn("Giới tính");
 
-  		int r = 0;
-     try
-     {
+  	int r = 0;
+     	try
+     	{
 
-     	//Class.forName("com.mysql.jdbc.Driver");
+     		//Class.forName("com.mysql.jdbc.Driver");
 		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
 		System.out.println("Connected to database.");
 		stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-        rs = stmt.executeQuery("select * from mobileshop.nhanvien");
-          while(rs.next())
-            {
-            	model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
+        	rs = stmt.executeQuery("select * from mobileshop.nhanvien");
+        	while(rs.next())
+            	{
+            		model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
 
-            }
+            	}
 
-             con.close();
-       }
-      catch(SQLException se)
-       {
-       	  System.out.println(se);
-          JOptionPane.showMessageDialog(null,"SQL Error:"+se);
-       }
-       catch(Exception e)
-       {
-       	   System.out.println(e);
-           JOptionPane.showMessageDialog(null,"Error:"+e);
-       }
+             	con.close();
+       	}
+      	catch(SQLException se)
+       	{
+       		System.out.println(se);
+          	JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+       	}
+       	catch(Exception e)
+       	{
+       	   	System.out.println(e);
+           	JOptionPane.showMessageDialog(null,"Error:"+e);
+       	}
 
 
-    jf.setTitle("Danh sách nhân viên");
-    jf.setSize(900,700);
+    	jf.setTitle("Danh sách nhân viên");
+    	jf.setSize(900,700);
 	jf.setLocation(20,20);
 	jf.setResizable(true);
-    jf.getContentPane().setBackground(Color.cyan);
-    jf.setVisible(true);
+    	jf.getContentPane().setBackground(Color.cyan);
+    	jf.setVisible(true);
   }
 
 
