a58668487c59fc629b59e228591ecce15316d41b
thanhcong1107 <92775736+thanhcong1107@users.noreply.github.com>
2022-09-02 22:13:32 +0700

    Update Quanly.java

--- src/LoginForm/Quanly.java
+++ src/LoginForm/Quanly.java
@@ -6,7 +6,8 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Quanly extends JFrame implements ActionListener
-{   JFrame jf;
+{   
+	JFrame jf;
 	JMenuBar mbar;
 	JMenu m1,m2,m3,m4,m5;
 	JMenuItem m1_1,m1_2,m1_3,m1_4,m1_5,m2_1,m2_2,m2_3,m2_4,m2_5,m3_1,m3_2,m3_3,m4_1,m4_2,m5_1;
@@ -15,7 +16,7 @@ public class Quanly extends JFrame implements ActionListener
 
 	public Quanly()
 	{
-        jf=new JFrame();
+        	jf=new JFrame();
 		gbl=new GridBagLayout();
 		jf.setLayout(gbl);
 
@@ -36,7 +37,7 @@ public class Quanly extends JFrame implements ActionListener
 		m1.add(m1_3);
 		m1_4 = new JMenuItem("Xóa nhân viên",new ImageIcon("images//delete.png"));
 		m1.add(m1_4);
-	    m1_5 = new JMenuItem("Danh sách nhân viên",new ImageIcon("images//list.png"));
+	    	m1_5 = new JMenuItem("Danh sách nhân viên",new ImageIcon("images//list.png"));
 		m1.add(m1_5);
 
 		m2=new JMenu("Quản lý sản phẩm");
@@ -49,13 +50,13 @@ public class Quanly extends JFrame implements ActionListener
 		m2.add(m2_3);
 		m2_4 = new JMenuItem("Xóa sản phẩm",new ImageIcon("images//delete.png"));
 		m2.add(m2_4);
-	    m2_5 = new JMenuItem("Danh sách sản phẩm",new ImageIcon("images//list.png"));
+	    	m2_5 = new JMenuItem("Danh sách sản phẩm",new ImageIcon("images//list.png"));
 		m2.add(m2_5);
 
 
 		m3=new JMenu("Quản lý danh mục");
-	    mbar.add(m3);
-	    m3_1 = new JMenuItem("Thêm danh mục",new ImageIcon("images//addnew.png"));
+	    	mbar.add(m3);
+	    	m3_1 = new JMenuItem("Thêm danh mục",new ImageIcon("images//addnew.png"));
 		m3.add(m3_1);
 		m3_2 = new JMenuItem("Chỉnh sửa thông tin danh mục",new ImageIcon("images//update.png"));
 		m3.add(m3_2);
@@ -73,17 +74,17 @@ public class Quanly extends JFrame implements ActionListener
 		m5_1 = new JMenuItem("Thoát",new ImageIcon("images//exit.png"));
 		m5.add(m5_1);
 
-        m1_1.addActionListener(this);
+        	m1_1.addActionListener(this);
 		m1_2.addActionListener(this);
 		m1_3.addActionListener(this);
 		m1_4.addActionListener(this);
-    	m1_5.addActionListener(this);
+    		m1_5.addActionListener(this);
 
 		m2_1.addActionListener(this);
 		m2_2.addActionListener(this);
 		m2_3.addActionListener(this);
 		m2_4.addActionListener(this);
-	    m2_5.addActionListener(this);
+	    	m2_5.addActionListener(this);
 
 		m3_1.addActionListener(this);
 		m3_2.addActionListener(this);
@@ -96,8 +97,8 @@ public class Quanly extends JFrame implements ActionListener
 
 		jf.setTitle("Quản lý");
 		jf.setLocation(20,20);
-	    jf.setSize(900,700);
-	    jf.setResizable(true);
+	    	jf.setSize(900,700);
+	    	jf.setResizable(true);
 		jf.getContentPane().setBackground(Color.cyan);
 		jf.setVisible(true);
 
@@ -122,7 +123,7 @@ public class Quanly extends JFrame implements ActionListener
 		{
 			new XoaNhanVien();
 		}
-	    else if(ae.getSource()==m1_5)
+	    	else if(ae.getSource()==m1_5)
 		{
 	    	new HienThiDanhSach();
 		}
