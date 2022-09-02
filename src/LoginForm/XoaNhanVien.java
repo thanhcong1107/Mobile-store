85937abe8a457f86a8e6cde4f097850f84c2b171
thanhcong1107 <92775736+thanhcong1107@users.noreply.github.com>
2022-09-02 23:02:32 +0700

    Update XoaNhanVien.java

--- src/LoginForm/XoaNhanVien.java
+++ src/LoginForm/XoaNhanVien.java
@@ -14,9 +14,9 @@ public class XoaNhanVien extends JFrame implements ActionListener
 	JLabel name,date,Sdt,address,ma_nv,chuc_vu,ma_chuc_vu,account,password,gioi_tinh,l11,ln;
 	JButton b_open,b_delete,b_clear;
 	JComboBox ChucVu,GioiTinh;
-    String s,sid1,sex;
+    	String s,sid1,sex;
 	Font f;
-    Connection con;
+    	Connection con;
 	PreparedStatement ps;
 	Statement stmt;
 	ResultSet rs;
@@ -25,9 +25,9 @@ public class XoaNhanVien extends JFrame implements ActionListener
 	 	       //all cells false
 	 	       return false;
 	 	    }
-	   };
-    JTable tabGrid = new JTable(model);
-    JScrollPane scrlPane = new JScrollPane(tabGrid);
+	};
+    	JTable tabGrid = new JTable(model);
+    	JScrollPane scrlPane = new JScrollPane(tabGrid);
 
 	XoaNhanVien()
 	{
@@ -35,11 +35,11 @@ public class XoaNhanVien extends JFrame implements ActionListener
 		f = new Font("Times New Roman",Font.BOLD,15);
 		jf.setLayout(null);
 
-	    ln=new JLabel("Xóa nhân viên");
-	    ln.setFont(new Font("Times New Roman",Font.BOLD,25));
-	    ln.setForeground(Color.blue);
-	    ln.setBounds(300,30,400,40);
-	    jf.add(ln);
+	    	ln=new JLabel("Xóa nhân viên");
+	    	ln.setFont(new Font("Times New Roman",Font.BOLD,25));
+	    	ln.setForeground(Color.blue);
+	    	ln.setBounds(300,30,400,40);
+	    	jf.add(ln);
 
 		name = new JLabel("Tên nhân viên");
 		name.setBounds(50,100,150,25);
@@ -53,7 +53,7 @@ public class XoaNhanVien extends JFrame implements ActionListener
 		date.setBounds(50,140,150,25);
 		jf.add(date);
 
-    	ngay_sinh = new JTextField(20);
+    		ngay_sinh = new JTextField(20);
 		ngay_sinh.setBounds(250,140,150,25);//ngay_sinh.setToolTipText("Điền ngày sinh theo định dạng DD/MM/YYYY");
 		jf.add(ngay_sinh);ngay_sinh.setEditable(false);
 
@@ -61,43 +61,41 @@ public class XoaNhanVien extends JFrame implements ActionListener
 		Sdt.setBounds(50,180,150,25);
 		jf.add(Sdt);
 
-     	sdt = new JTextField(20);
+     		sdt = new JTextField(20);
 		sdt.setBounds(250,180,150,25);//sdt.setToolTipText("Điền số điện thoại");
 		jf.add(sdt);sdt.setEditable(false);
 
 		address = new JLabel("Địa chỉ");
 		address.setBounds(50,220,150,25);
-    	jf.add(address);
+    		jf.add(address);
 
-        dia_chi= new JTextField(20);
+        	dia_chi= new JTextField(20);
 		dia_chi.setBounds(250,220,150,25);//dia_chi.setToolTipText("Điền địa chỉ ");
 		jf.add(dia_chi);dia_chi.setEditable(false);
 		
 		gioi_tinh = new JLabel("Giới tính");
 		gioi_tinh.setBounds(50,260,150,25);
-    	jf.add(gioi_tinh);
+    		jf.add(gioi_tinh);
     	
-    	GioiTinh=new JComboBox();
-    	GioiTinh.setBounds(250,260,150,25);
-    	jf.add(GioiTinh);
-        GioiTinh.addItem("---");
+    		GioiTinh=new JComboBox();
+    		GioiTinh.setBounds(250,260,150,25);
+    		jf.add(GioiTinh);
+        	GioiTinh.addItem("---");
 		GioiTinh.addItem("Nam");
 		GioiTinh.addItem("Nữ");
 		GioiTinh.addActionListener(new ActionListener()
-	    {
-		   public void actionPerformed(ActionEvent ae)
-		   {
-			   sex =(String)GioiTinh.getSelectedItem();
-			   
-               
-		   }
-        });
+	    	{
+		   	public void actionPerformed(ActionEvent ae)
+		   	{
+			   	sex =(String)GioiTinh.getSelectedItem();
+		   	}
+        	});
 		GioiTinh.setEditable(false);
 
 //Cột 2
 		ma_nv = new JLabel("Mã nhân viên");
 		ma_nv.setBounds(470,100,150,25);
-    	jf.add(ma_nv);
+    		jf.add(ma_nv);
     	
 		ma_NV= new JTextField(20);
 		ma_NV.setBounds(670,100,150,25);ma_NV.setToolTipText("Điền mã nhân viên, ví dụ Nguyễn Văn Anh, ngày sinh 01/01/2000 -> Mã NV : Anh.NV0101");
@@ -105,23 +103,23 @@ public class XoaNhanVien extends JFrame implements ActionListener
 
 		account= new JLabel("Tài khoản");
 		account.setBounds(470,220,150,25);
-    	jf.add(account);
+    		jf.add(account);
 
-        tai_khoan= new JTextField(20);
+        	tai_khoan= new JTextField(20);
 		tai_khoan.setBounds(670,220,150,25);
 		jf.add(tai_khoan);tai_khoan.setEditable(false);
 
 		password= new JLabel("Mật khẩu");
 		password.setBounds(470,260,150,25);
-    	jf.add(password);
+    		jf.add(password);
     	
-    	mat_khau= new JTextField(20);
+    		mat_khau= new JTextField(20);
 		mat_khau.setBounds(670,260,100,25);
 		jf.add(mat_khau);mat_khau.setEditable(false);
 
 		ma_chuc_vu= new JLabel("Mã chức vụ");
 		ma_chuc_vu.setBounds(470,180,150,25);
-    	jf.add(ma_chuc_vu);
+    		jf.add(ma_chuc_vu);
     	
 		ma_CV= new JTextField(20);
 		ma_CV.setBounds(670,180,150,25);
@@ -129,221 +127,221 @@ public class XoaNhanVien extends JFrame implements ActionListener
 		
 		chuc_vu= new JLabel("Chức vụ");
 		chuc_vu.setBounds(470,140,150,25);
-    	jf.add(chuc_vu);
+    		jf.add(chuc_vu);
     	
-        ChucVu=new JComboBox();
-        ChucVu.addItem("---");
-        ChucVu.addItem("Quản lý");
+        	ChucVu=new JComboBox();
+        	ChucVu.addItem("---");
+        	ChucVu.addItem("Quản lý");
 		ChucVu.addItem("Nhân viên bán hàng");
 		
 		ChucVu.setBounds(670,140,150,25);ChucVu.setToolTipText("Chọn chức vụ");
 		jf.add(ChucVu);
 		ChucVu.addActionListener(new ActionListener()
-	    {
-		   public void actionPerformed(ActionEvent ae)
-		   {
-			   String Chuc_Vu =(String)ChucVu.getSelectedItem();
-			   if (Chuc_Vu.equalsIgnoreCase("Quản lý")) {
-                   ma_CV.setText("QL");
-                   ma_CV.setEditable(false);
-               }
-			   else if (Chuc_Vu.equalsIgnoreCase("Nhân viên bán hàng")) {
-                   ma_CV.setText("NVBH");
-                   ma_CV.setEditable(false);
-               }
-		   }
-        });
+	   	{
+		   	public void actionPerformed(ActionEvent ae)
+		   	{
+			   	String Chuc_Vu =(String)ChucVu.getSelectedItem();
+			   	if (Chuc_Vu.equalsIgnoreCase("Quản lý")) {
+                  		 	ma_CV.setText("QL");
+                   			ma_CV.setEditable(false);
+               			}
+			   	else if (Chuc_Vu.equalsIgnoreCase("Nhân viên bán hàng")) {
+                   			ma_CV.setText("NVBH");
+                   			ma_CV.setEditable(false);
+               			}
+		  	}
+        	});
 		ChucVu.setEditable(false);
 
 		b_open = new JButton("Mở",new ImageIcon("images//open.png"));
-        b_open.setBounds(150,330,110,35);b_open.setToolTipText("Click để hiện thị toàn bộ thông tin nhân viên");
+        	b_open.setBounds(150,330,110,35);b_open.setToolTipText("Click để hiện thị toàn bộ thông tin nhân viên");
 		jf.add(b_open);b_open.addActionListener(this);
 		
 		b_delete = new JButton("Xóa",new ImageIcon("images//delete.png"));
-        b_delete.setBounds(350,330,110,35);b_delete.setToolTipText("Click để xóa nhân viên");
+        	b_delete.setBounds(350,330,110,35);b_delete.setToolTipText("Click để xóa nhân viên");
 		jf.add(b_delete);b_delete.addActionListener(this);
 
 		b_clear = new JButton("Làm mới",new ImageIcon("images//clear.png"));
 		b_clear.setBounds(550,330,110,35);b_clear.setToolTipText("Click để xóa toàn bộ thông tin đã điền");
-	    jf.add(b_clear); b_clear.addActionListener(this);
+	    	jf.add(b_clear); b_clear.addActionListener(this);
 
         
 
-	    scrlPane.setBounds(0,380,900,600);
-        jf.add(scrlPane);
-        tabGrid.setFont(new Font ("Times New Roman",0,15));
+	    	scrlPane.setBounds(0,380,900,600);
+        	jf.add(scrlPane);
+        	tabGrid.setFont(new Font ("Times New Roman",0,15));
 
-        model.addColumn("Tên nhân viên");
-        model.addColumn("Ngày sinh");
-        model.addColumn("Số điện thoại");
-        model.addColumn("Địa chỉ");
-        model.addColumn("Mã nhân viên");
-        model.addColumn("Chức vụ");
-        model.addColumn("Mã chức vụ");
-        model.addColumn("Tài khoản");
-        model.addColumn("Mật khẩu");
-        int r = 0;
-        try
-        {
+        	model.addColumn("Tên nhân viên");
+        	model.addColumn("Ngày sinh");
+        	model.addColumn("Số điện thoại");
+        	model.addColumn("Địa chỉ");
+       	 	model.addColumn("Mã nhân viên");
+        	model.addColumn("Chức vụ");
+        	model.addColumn("Mã chức vụ");
+        	model.addColumn("Tài khoản");
+        	model.addColumn("Mật khẩu");
+        	int r = 0;
+        	try
+		{
 
-        	//Class.forName("com.mysql.jdbc.Driver");
-        	con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
-        	System.out.println("Connected to database.");
-        	stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-        	rs = stmt.executeQuery("select * from mobileshop.nhanvien");
-            while(rs.next())
-               {
-            		model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
+        		//Class.forName("com.mysql.jdbc.Driver");
+        		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
+        		System.out.println("Connected to database.");
+        		stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        		rs = stmt.executeQuery("select * from mobileshop.nhanvien");
+           	 	while(rs.next())
+               		{
+            			model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
 
-               }
+               		}
 
-                con.close();
-          }
-         catch(SQLException se)
-          {
-          	  System.out.println(se);
-             JOptionPane.showMessageDialog(null,"SQL Error:"+se);
-          }
-          catch(Exception e)
-          {
-          	   System.out.println(e);
-              JOptionPane.showMessageDialog(null,"Error:"+e);
-          }
+                	con.close();
+          	}
+         	catch(SQLException se)
+          	{
+          	  	System.out.println(se);
+		     	JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+          	}
+          	catch(Exception e)
+          	{
+          	   	System.out.println(e);
+              		JOptionPane.showMessageDialog(null,"Error:"+e);
+          	}
 
-	     jf.setTitle("Xóa nhân viên ");
-	     jf.setSize(900,700);
-		 jf.setLocation(20,20);
-		 jf.setResizable(false);
-		 jf.getContentPane().setBackground(Color.cyan);
-	     jf.setVisible(true);
- }
+	     	jf.setTitle("Xóa nhân viên ");
+	     	jf.setSize(900,700);
+		jf.setLocation(20,20);
+		jf.setResizable(false);
+		jf.getContentPane().setBackground(Color.cyan);
+	     	jf.setVisible(true);
+ 	}
 
- public void actionPerformed(ActionEvent ae)
+ 	public void actionPerformed(ActionEvent ae)
  	{
   		if(ae.getSource()==b_open)
   		{
   			if((ma_NV.getText()).equals(""))
-	        {
-	  	 		 JOptionPane.showMessageDialog(this,"Để xóa nhân viên, hãy nhập mã nhân viên!","Warning!!!",JOptionPane.WARNING_MESSAGE);
-	        }
-	        else
-	        {
+	        	{
+	  	 		JOptionPane.showMessageDialog(this,"Để xóa nhân viên, hãy nhập mã nhân viên!","Warning!!!",JOptionPane.WARNING_MESSAGE);
+	        	}
+	        	else
+	        	{
 
-	        	try
-	        	{//fetch
-	        		int foundrec = 0;
-	        		//Class.forName("com.mysql.jdbc.Driver");
-	        		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
-	        		System.out.println("Connected to database.");
+	        		try
+	        		{//fetch
+	        			int foundrec = 0;
+	        			//Class.forName("com.mysql.jdbc.Driver");
+	        			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
+	        			System.out.println("Connected to database.");
 	    	  
-	        		ps=con.prepareStatement("select * from mobileshop.nhanvien where maNV='"+ma_NV.getText()+"'");
-	        		rs=ps.executeQuery();
-	        		while(rs.next())
-	        		{
-	        			ten.setText(rs.getString(1));
-	        			ngay_sinh.setText(rs.getString(2));
-	        			sdt.setText(rs.getString(3));
-	        			dia_chi.setText(rs.getString(4));
-	        			ma_NV.setText(rs.getString(5));
-	        			ChucVu.setSelectedItem(rs.getString(6));
-	        			ma_CV.setText(rs.getString(7));
-	        			tai_khoan.setText(rs.getString(8));
-	        			mat_khau.setText(rs.getString(9));
-	        			GioiTinh.setSelectedItem(rs.getString(10));
-	        			foundrec = 1;
-	        		}
-	        		if (foundrec == 0)
-	        		{
-	        			JOptionPane.showMessageDialog(null,"Nhân viên không tồn tại, vui lòng kiểm tra lại thông tin","Dialogs",JOptionPane.WARNING_MESSAGE);
-	        		}
+	        			ps=con.prepareStatement("select * from mobileshop.nhanvien where maNV='"+ma_NV.getText()+"'");
+	        			rs=ps.executeQuery();
+	        			while(rs.next())
+	        			{
+	        				ten.setText(rs.getString(1));
+	        				ngay_sinh.setText(rs.getString(2));
+	        				sdt.setText(rs.getString(3));
+	        				dia_chi.setText(rs.getString(4));
+	        				ma_NV.setText(rs.getString(5));
+	        				ChucVu.setSelectedItem(rs.getString(6));
+	        				ma_CV.setText(rs.getString(7));
+	        				tai_khoan.setText(rs.getString(8));
+	        				mat_khau.setText(rs.getString(9));
+	        				GioiTinh.setSelectedItem(rs.getString(10));
+	        				foundrec = 1;
+	        			}
+	        			if (foundrec == 0)
+	        			{
+	        				JOptionPane.showMessageDialog(null,"Nhân viên không tồn tại, vui lòng kiểm tra lại thông tin","Dialogs",JOptionPane.WARNING_MESSAGE);
+	        			}
 
-	        		con.close();
-	        	}
-	        	catch(SQLException se)
-	        	{
-	        		System.out.println(se);
-	        		JOptionPane.showMessageDialog(null,"SQL Error:"+se);
-	        	}
-	        	catch(Exception e)
-	        	{
-	        		System.out.println(e);
-	        		JOptionPane.showMessageDialog(null,"Error:"+e);
-	        	}
-	        }
+	        			con.close();
+	        		}
+	        		catch(SQLException se)
+	        		{
+	        			System.out.println(se);
+	        			JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+	        		}
+	        		catch(Exception e)
+	        		{
+	        			System.out.println(e);
+	        			JOptionPane.showMessageDialog(null,"Error:"+e);
+	        		}
+	       	 	}
   		}
   		else if(ae.getSource()==b_delete)
   		{
   			if(((ma_NV.getText()).equals("")))
-  		     {
+  		     	{
   				JOptionPane.showMessageDialog(this,"Hãy điền mã nhân viên !","Warning!!!",JOptionPane.WARNING_MESSAGE);
-  		     }
+  		     	}
   			
-	        else
-	        { 
-	        try 
-	        {
-			//Class.forName("com.mysql.jdbc.Driver");
-	        	con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
-	        	System.out.println("Connected to database.");
-	        	stmt=con.createStatement();
-	        	ps=con.prepareStatement("delete from mobileshop.nhanvien where name='"+ten.getText()+"' or MaNV='"+ma_NV.getText()+"'");
-	        	ps.executeUpdate();
-	        	JOptionPane.showMessageDialog(null, "Nhân viên đã bị xóa");
-	        	for(int c=model.getRowCount()-1;c>=0;c--)
-	        	{
-	        		model.removeRow(c);
-	        	}
-	        	rs = stmt.executeQuery("select * from mobileshop.nhanvien");
-	        	int r=0;
-	        	while(rs.next())
-	        	{
-	        		model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
+	       		else
+	        	{ 
+	       	 		try 
+	        		{
+					//Class.forName("com.mysql.jdbc.Driver");
+	        			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mobileshop","root","thanhcong1107");
+	        			System.out.println("Connected to database.");
+	        			stmt=con.createStatement();
+	        			ps=con.prepareStatement("delete from mobileshop.nhanvien where name='"+ten.getText()+"' or MaNV='"+ma_NV.getText()+"'");
+	        			ps.executeUpdate();
+	        			JOptionPane.showMessageDialog(null, "Nhân viên đã bị xóa");
+	        			for(int c=model.getRowCount()-1;c>=0;c--)
+	        			{
+	        				model.removeRow(c);
+	        			}
+	        			rs = stmt.executeQuery("select * from mobileshop.nhanvien");
+	        			int r=0;
+	        			while(rs.next())
+	        			{
+	        				model.insertRow(r++,new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10)});
 
-	            }
-	        	ten.setText("");
-	        	ngay_sinh.setText("");
-	        	sdt.setText("");
-	        	dia_chi.setText("");
-	        	ma_NV.setText("");
-	        	ChucVu.setSelectedItem("---");
-	        	ma_CV.setText("");
-	        	tai_khoan.setText("");
-	        	mat_khau.setText("");
-	        	GioiTinh.setSelectedItem("---");
-	        	con.close();
-	        }
+	           			 }
+	        			ten.setText("");
+	        			ngay_sinh.setText("");
+	        			sdt.setText("");
+	        			dia_chi.setText("");
+	        			ma_NV.setText("");
+	        			ChucVu.setSelectedItem("---");
+	        			ma_CV.setText("");
+	        			tai_khoan.setText("");
+	        			mat_khau.setText("");
+	        			GioiTinh.setSelectedItem("---");
+	        			con.close();
+	        		}
 	   
-	   catch(SQLException se)
-	   {
-		   System.out.println(se);
-		   JOptionPane.showMessageDialog(null,"SQL Error:"+se);
-	   }
-	   catch(Exception e)
-	   {
-		   System.out.println(e);
-		   JOptionPane.showMessageDialog(null,"Error:"+e);
-	   }
-	   }
-  	}
-  else if(ae.getSource()==b_clear)
-      {    
-	  	ten.setText("");
-		ngay_sinh.setText("");
-		sdt.setText("");
-		dia_chi.setText("");
-		ma_NV.setText("");
-		ChucVu.setSelectedItem("---");
-		ma_CV.setText("");
-		tai_khoan.setText("");
-		mat_khau.setText("");
-		GioiTinh.setSelectedItem("---");
-     }
+	   			catch(SQLException se)
+	   			{
+		   			System.out.println(se);
+		   J			OptionPane.showMessageDialog(null,"SQL Error:"+se);
+				 }
+			   	catch(Exception e)
+	   			{
+		   			System.out.println(e);
+		   			JOptionPane.showMessageDialog(null,"Error:"+e);
+	   			}
+	  		 }
+  		}
+  		else if(ae.getSource()==b_clear)
+      		{    
+	  		ten.setText("");
+			ngay_sinh.setText("");
+			sdt.setText("");
+			dia_chi.setText("");
+			ma_NV.setText("");
+			ChucVu.setSelectedItem("---");
+			ma_CV.setText("");
+			tai_khoan.setText("");
+			mat_khau.setText("");
+			GioiTinh.setSelectedItem("---");
+     		}
  
-  }
- public static void main(String args[])
- {
-	 new XoaNhanVien();
- }
+  		}
+ 	public static void main(String args[])
+ 	{
+	 	new XoaNhanVien();
+ 	}
 }
 
 
